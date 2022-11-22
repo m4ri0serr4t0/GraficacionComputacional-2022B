76d88360c54079ad3ec558882a04085b77d9a79a
Edson Reza <edson.reza@hotmail.com>
2022-11-22 11:37:10 -0600

    Cambios menores

--- Edson-Elias-Reza-Garduno/b-splines/src/graphic/plotting.rs
+++ Edson-Elias-Reza-Garduno/b-splines/src/graphic/plotting.rs
@@ -1,108 +1,147 @@
-use plotters::prelude::*;
-use crate::common::vec_point::Vec;
 use crate::common::vec_point::Point;
-use crate::common::vec_point::Transform;
 use crate::common::vec_point::RangeXY;
+use crate::common::vec_point::Transform;
+use crate::common::vec_point::Vec;
 use crate::common::vec_point3d::Point3D;
 use crate::common::vec_point3d::Transform as Transform3D;
+use plotters::prelude::*;
 use std::ops::Range;
 
-pub fn plot_curve(title:&str, points:&Vec<Point>, curve:&Vec<Point>) {
-	const WIDTH_IMG:u32 = 1200;
-	const HEIGHT_IMG:u32 = 1200;
+pub fn plot_curve(title: &str, points: &Vec<Point>, curve: &Vec<Point>) {
+    const WIDTH_IMG: u32 = 1200;
+    const HEIGHT_IMG: u32 = 1200;
     let drawing_area = BitMapBackend::new(title, (WIDTH_IMG, HEIGHT_IMG)).into_drawing_area();
+    drawing_area.fill(&WHITE).unwrap();
+
+    let x_range: Range<f64> = points.x_range();
+    let y_range: Range<f64> = points.y_range();
+
+    let width: Range<f64> = Range {
+        start: 0.0,
+        end: x_range.end.ceil() + 0.5,
+    };
+
+    let height = Range {
+        start: 0.0,
+        end: y_range.end.ceil() + 0.5,
+    };
+
+    let mut chart = ChartBuilder::on(&drawing_area)
+        .build_cartesian_2d(width, height)
+        .unwrap();
+    chart.configure_mesh().draw().unwrap();
+
+    chart
+        .draw_series(LineSeries::new(curve.to_tuples(), &RED))
+        .unwrap();
+
+    chart
+        .draw_series(PointSeries::of_element(
+            points.to_tuples(),
+            5,
+            &GREEN,
+            &|c, s, st| {
+                return EmptyElement::at(c)    // We want to construct a composed element on-the-fly
+				+ Circle::new((0,0),s,st.filled()) // At this point, the new pixel coordinate is established
+				+ Text::new(format!("{:?}", c), (10, 0), ("sans-serif", 10).into_font());
+            },
+        ))
+        .unwrap();
+
+    drawing_area.present().expect("No se pudo crear el archivo");
+    println!("La curva se ha guardado en {}", title);
+}
+
+pub fn plot_polygon(title: &str, points: &Vec<Point>, curve: &Vec<Point>) {
+	let drawing_area = BitMapBackend::new(title, (800, 800)).into_drawing_area();
 	drawing_area.fill(&WHITE).unwrap();
 
-	let x_range: Range<f64> = points.x_range();
-	let y_range: Range<f64> = points.y_range();
+	let x_range = points.x_range();
+	let y_range = points.y_range();
 
-	let width: Range<f64> = Range {
+	let width = Range {
 		start: 0.0, 
-		end: x_range.end.ceil() + 0.5
+		end: x_range.end.ceil() + 1.0
 	};
 
 	let height = Range {
 		start: 0.0, 
-		end: y_range.end.ceil() + 0.5
+		end: y_range.end.ceil() + 1.0
 	};
 
 	let mut chart = ChartBuilder::on(&drawing_area)
 		.build_cartesian_2d(width, height)
 		.unwrap();
 
-	// CURVA
-	chart.draw_series(
-		LineSeries::new(curve.to_tuples(), &BLACK)
-	).unwrap();
+	chart.draw_series(std::iter::once(Polygon::new(curve.to_tuples().clone(), &BLACK,))).unwrap();
 
+	println!("\tCreando archivo {}...", title);
 	drawing_area.present().expect("No se pudo crear el archivo");
-	println!("La curva se ha guardado en {}", title);
 }
 
-pub fn plot_surface(title:&str, points:&Vec<Vec<Point3D>>, surface:&Vec<Point3D>) {
-	const WIDTH_IMG:u32 = 1200;
-	const HEIGHT_IMG:u32 = 1200;
-	let drawing_area = BitMapBackend::new(title, (WIDTH_IMG, HEIGHT_IMG)).into_drawing_area();
-	drawing_area.fill(&WHITE).unwrap();
+pub fn plot_surface(title: &str, points: &Vec<Vec<Point3D>>, surface: &Vec<Point3D>) {
+    const WIDTH_IMG: u32 = 1200;
+    const HEIGHT_IMG: u32 = 1200;
+    let drawing_area = BitMapBackend::new(title, (WIDTH_IMG, HEIGHT_IMG)).into_drawing_area();
+    drawing_area.fill(&WHITE).unwrap();
 
-	let mut min_point: Point3D = points[0][0];
-	let mut max_point: Point3D = points[0][0];
+    let mut min_point: Point3D = points[0][0];
+    let mut max_point: Point3D = points[0][0];
 
-	for i in points {
-		for j in i {
-			if j.x < min_point.x {
-				min_point.x = j.x;
-			}
-			if j.y < min_point.y {
-				min_point.y = j.y;
-			}
-			if j.z < min_point.z {
-				min_point.z = j.z;
-			}
-			if j.x > max_point.x {
-				max_point.x = j.x;
-			}
-			if j.y > max_point.y {
-				max_point.y = j.y;
-			}
-			if j.z > max_point.z {
-				max_point.z = j.z;
-			}
-		}
-	}
+    for i in points {
+        for j in i {
+            if j.x < min_point.x {
+                min_point.x = j.x;
+            }
+            if j.y < min_point.y {
+                min_point.y = j.y;
+            }
+            if j.z < min_point.z {
+                min_point.z = j.z;
+            }
+            if j.x > max_point.x {
+                max_point.x = j.x;
+            }
+            if j.y > max_point.y {
+                max_point.y = j.y;
+            }
+            if j.z > max_point.z {
+                max_point.z = j.z;
+            }
+        }
+    }
 
-	let width: Range<f64> = Range {
-		start: min_point.x - 0.5, 
-		end: max_point.x + 0.5
-	};
+    let width: Range<f64> = Range {
+        start: min_point.x - 0.5,
+        end: max_point.x + 0.5,
+    };
 
-	let height = Range {
-		start: min_point.y - 0.5, 
-		end: max_point.y + 0.5
-	};
+    let height = Range {
+        start: min_point.y - 0.5,
+        end: max_point.y + 0.5,
+    };
 
-	let z = Range {
-		start: min_point.z - 0.5, 
-		end: max_point.z + 0.5
-	};
+    let z = Range {
+        start: min_point.z - 0.5,
+        end: max_point.z + 0.5,
+    };
 
-	let mut chart = ChartBuilder::on(&drawing_area)
-		.build_cartesian_3d(width, height, z)
+    let mut chart = ChartBuilder::on(&drawing_area)
+        .build_cartesian_3d(width, height, z)
+        .unwrap();
+
+    chart
+        .configure_axes()
+        .light_grid_style(BLACK.mix(0.15))
+        .max_light_lines(3)
+        .draw()
 		.unwrap();
 
-	chart
-		.configure_axes()
-		.light_grid_style(BLACK.mix(0.15))
-		.max_light_lines(3)
-		.draw();
+    // SUPERFICIE
+    chart
+        .draw_series(LineSeries::new(surface.to_tuples(), &BLACK))
+        .unwrap();
 
-	// SUPERFICIE
-	chart.draw_series(
-		LineSeries::new(surface.to_tuples(), &BLACK)
-	).unwrap();
-
-
-
-	drawing_area.present().expect("No se pudo crear el archivo");
-	println!("La curva se ha guardado en {}", title);
+    drawing_area.present().expect("No se pudo crear el archivo");
+    println!("La curva se ha guardado en {}", title);
 }
--- Edson-Elias-Reza-Garduno/b-splines/src/main.rs
+++ Edson-Elias-Reza-Garduno/b-splines/src/main.rs
@@ -1,7 +1,6 @@
 use b_splines::common::point::Point;
 use b_splines::common::read;
 use b_splines::common::read::read_vector;
-use b_splines::common::vec_point3d::Point3D;
 use b_splines::common::verification;
 use b_splines::graphic::bspline;
 use b_splines::graphic::plotting as plot;
@@ -14,14 +13,12 @@ fn main() {
     println!("Bienvenido al programa de generación de B-Splines");
     println!("Seleccione la opción que desea realizar");
     println!("1. Generar una curva");
-    println!("2. Generar una superficie");
     println!("Otro. Salir");
     print!("Opción: ");
     let opcion: i8 = read::read();
 
     match opcion {
         1 => curve(),
-        2 => surface_example(),
         _ => println!("Hasta luego"),
     }
 }
@@ -55,136 +52,4 @@ fn curve() {
     name = verification::nombre_con_extension(&name, ".png");
 
     plot::plot_curve(&name, &points, &bspline);
-}
-
-fn surface() {
-    // Lectura de nudos en U
-    print!("Ingrese el numero de nudos en u: ");
-    let n: i32 = read::read();
-    let knots_u: Vec<f64> = read_vector(n);
-
-    // Lectura de nudos en V
-    print!("Ingrese el numero de nudos en v: ");
-    let n: i32 = read::read();
-    let knots_v: Vec<f64> = read_vector(n);
-
-    // Lectura de puntos de control
-    println!("Considere que los puntos de control se colocan en una matriz de n x m");
-    print!("Ingrese el numero de columnas: ");
-    let n: i32 = read::read();
-    print!("Ingrese el numero de filas: ");
-    let m: i32 = read::read();
-    let points: Vec<Vec<Point3D>> = read::read_matrix(n, m);
-
-    // Lectura del grado en U
-    print!("Ingrese el grado en u: ");
-    let p: i32 = read::read();
-
-    // Lectura del grado en V
-    print!("Ingrese el grado en v: ");
-    let q: i32 = read::read();
-
-    // Lectura de saltos en la superficie, define la resolucion de la superficie
-    let u: f64 = 0.01;
-    let v: f64 = 0.01;
-
-    // Generacion de la superficie
-    let bspline: Vec<Point3D> = bspline::bspline_surface(&knots_u, &knots_v, &points, u, v, p, q);
-
-    print!("Ingrese el nombre del archivo para guardar el resultado (.png): ");
-    let mut name: String = read::read();
-    name = verification::nombre_con_extension(&name, ".png");
-
-    plot::plot_surface(&name, &points, &bspline);
-}
-
-fn surface_example() {
-    // * This is an example of the book "The NURBS Book" by Piegl and Tiller.
-
-    // * Knots in U
-    let knots_u: Vec<f64> = vec![0.0, 0.0, 0.0, 0.25, 0.5, 0.75, 1.0, 1.0, 1.0];
-
-    // * Knots in V
-    let knots_v: Vec<f64> = vec![0.0, 0.0, 0.0, 0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.0, 1.0, 1.0];
-
-    // * Control points
-    let points: Vec<Vec<Point3D>> = vec![
-        vec![
-            Point3D::new(0.0, 0.0, 0.0),
-            Point3D::new(0.0, 1.0, 0.0),
-            Point3D::new(0.0, 2.0, 0.0),
-            Point3D::new(0.0, 3.0, 0.0),
-            Point3D::new(0.0, 4.0, 0.0),
-            Point3D::new(0.0, 5.0, 0.0),
-            Point3D::new(0.0, 6.0, 0.0),
-            Point3D::new(0.0, 7.0, 0.0),
-
-        ],
-        vec![
-            Point3D::new(1.0, 0.0, 0.0),
-            Point3D::new(1.0, 1.0, 0.0),
-            Point3D::new(1.0, 2.0, 0.0),
-            Point3D::new(1.0, 3.0, 0.0),
-            Point3D::new(1.0, 4.0, 0.0),
-            Point3D::new(1.0, 5.0, 0.0),
-            Point3D::new(1.0, 6.0, 0.0),
-            Point3D::new(1.0, 7.0, 0.0),
-        ],
-        vec![
-            Point3D::new(2.0, 0.0, 0.0),
-            Point3D::new(2.0, 1.0, 0.0),
-            Point3D::new(2.0, 2.0, 0.0),
-            Point3D::new(2.0, 3.0, 0.0),
-            Point3D::new(2.0, 4.0, 0.0),
-            Point3D::new(2.0, 5.0, 0.0),
-            Point3D::new(2.0, 6.0, 0.0),
-            Point3D::new(2.0, 7.0, 0.0),
-        ],
-        vec![
-            Point3D::new(3.0, 0.0, 0.0),
-            Point3D::new(3.0, 1.0, 0.0),
-            Point3D::new(3.0, 2.0, 0.0),
-            Point3D::new(3.0, 3.0, 0.0),
-            Point3D::new(3.0, 4.0, 0.0),
-            Point3D::new(3.0, 5.0, 0.0),
-            Point3D::new(3.0, 6.0, 0.0),
-            Point3D::new(3.0, 7.0, 0.0),
-        ],
-        vec![
-            Point3D::new(4.0, 0.0, 0.0),
-            Point3D::new(4.0, 1.0, 0.0),
-            Point3D::new(4.0, 2.0, 0.0),
-            Point3D::new(4.0, 3.0, 0.0),
-            Point3D::new(4.0, 4.0, 0.0),
-            Point3D::new(4.0, 5.0, 0.0),
-            Point3D::new(4.0, 6.0, 0.0),
-            Point3D::new(4.0, 7.0, 0.0),
-        ],
-        vec![
-            Point3D::new(5.0, 0.0, 0.0),
-            Point3D::new(5.0, 1.0, 0.0),
-            Point3D::new(5.0, 2.0, 0.0),
-            Point3D::new(5.0, 3.0, 0.0),
-            Point3D::new(5.0, 4.0, 0.0),
-            Point3D::new(5.0, 5.0, 0.0),
-            Point3D::new(5.0, 6.0, 0.0),
-            Point3D::new(5.0, 7.0, 0.0),
-        ],
-    ];
-
-    // * Degree in U
-    let p: i32 = 2;
-
-    // * Degree in V
-    let q: i32 = 3;
-
-    // * Steps in the surface, defines the resolution of the surface
-    let u: f64 = 0.01;
-    let v: f64 = 0.01;
-
-    // * Generation of the surface
-    let bspline: Vec<Point3D> = bspline::bspline_surface(&knots_u, &knots_v, &points, u, v, p, q);
-
-    // * Drawing of the surface
-    plot::plot_surface("surface.png", &points, &bspline);
 }
